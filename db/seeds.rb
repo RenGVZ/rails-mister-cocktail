9c8694424aabe6e420a87a2cd74b630765344f8a
Gavin Garza <gaving@live.com>
2020-01-20 15:25:33 +0900

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
 require 'json'
 require 'open-uri'
 
+Cocktail.destroy_all
+
+file = URI.open('https://images.unsplash.com/photo-1556679343-c7306c1976bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80')
+cocktail = Cocktail.new(name: "Cuba Libre")
+cocktail.photo.attach(io: file, filename: 'cuba-img', content_type: 'image/jpg')
+cocktail.save
+
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients_serialized = open(url).read
 ingredients_json = JSON.parse(ingredients_serialized)
@@ -8,6 +15,3 @@ ingredients = ingredients_json["drinks"]
 ingredients.each do |ingredient|
   Ingredient.create(name: ingredient["strIngredient1"])
 end
-
-
-
