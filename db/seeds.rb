e102e7b51a16a973fae64accba246b51a11aaf01
gavingarza <gaving@live.com>
2020-05-04 01:03:53 +0900

    re-seeding the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,31 @@ cocktail = Cocktail.new(name: "Cuba Libre")
 cocktail.photo.attach(io: file, filename: 'cuba-img', content_type: 'image/jpg')
 cocktail.save
 
+file = URI.open('https://source.unsplash.com/lwoTuByIuC4')
+cocktail = Cocktail.new(name: "Mint Julep")
+cocktail.photo.attach(io: file, filename: 'julep-img', content_type: 'image/jpg')
+cocktail.save
+
+file = URI.open('https://source.unsplash.com/dmkmrNptMpw')
+cocktail = Cocktail.new(name: "Old Fashioned")
+cocktail.photo.attach(io: file, filename: 'old-img', content_type: 'image/jpg')
+cocktail.save
+
+file = URI.open('https://source.unsplash.com/T7heq8rawkc')
+cocktail = Cocktail.new(name: "Mai Tai")
+cocktail.photo.attach(io: file, filename: 'mai-img', content_type: 'image/jpg')
+cocktail.save
+
+file = URI.open('https://source.unsplash.com/7hHRTw_-1SY')
+cocktail = Cocktail.new(name: "Lemon Sour")
+cocktail.photo.attach(io: file, filename: 'sour-img', content_type: 'image/jpg')
+cocktail.save
+
+file = URI.open('https://source.unsplash.com/2ssSOO2pPK8')
+cocktail = Cocktail.new(name: "Martini")
+cocktail.photo.attach(io: file, filename: 'martini-img', content_type: 'image/jpg')
+cocktail.save
+
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients_serialized = open(url).read
 ingredients_json = JSON.parse(ingredients_serialized)
